964359daae18a979073635c112dca8881194e93b
sebastian-raubach <sebastian.raubach@hutton.ac.uk>
2022-06-19 15:51:30 +0100

    - CHG: Updated service worker.

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -21,6 +21,18 @@ if (process.env.NODE_ENV === 'production') {
     },
     updated () {
       console.log('New content is available; please refresh.')
+
+      if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.getRegistrations().then(function (registrations) {
+          for (const registration of registrations) {
+            registration.update()
+          }
+        })
+      }
+
+      setTimeout(() => {
+        window.location.reload(true)
+      }, 1000)
     },
     offline () {
       console.log('No internet connection found. App is running in offline mode.')
