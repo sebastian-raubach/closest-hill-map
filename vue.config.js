533856793aebb846e333311ea4e1faf59fb379ab
sebastian-raubach <sebastian.raubach@hutton.ac.uk>
2022-06-20 08:07:51 +0100

    - ADD: Favicon. - ADD: Site manifest. - ADD: PWA config.

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,17 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  pwa: {
+    name: 'ClosestHillMap',
+    themeColor: '#20bf6b',
+    msTileColor: '#ffffff',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    start_url: '/index.html',
+    workboxOptions: {
+      skipWaiting: true
+    }
+  },
   configureWebpack: {
     // plugins: [
     //   new BundleAnalyzerPlugin()
